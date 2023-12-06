5a7c40577c8c9836cc4ae0bbec01f4b20545e16c
Jesse Dijkstra <mail@jessedijkstra.nl>
2023-12-06 17:17:21 +0100

    Remove image plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,7 +13,6 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-image",
     "gatsby-plugin-postcss",
     {
       resolve: "gatsby-plugin-manifest",
