c7b34cd3035a3a227865ed97c426d2b26a42b14b
Jesse Dijkstra <mail@jessedijkstra.nl>
2025-11-15 13:56:11 +0100

    Remove solidbase

--- app.config.ts
+++ app.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from "@solidjs/start/config";
-import { withSolidBase } from "@kobalte/solidbase/config";
+
 
 export default defineConfig(
-  withSolidBase(
-    // SolidStart config
-    {
-      server: {
-        prerender: {
-          crawlLinks: true,
-        },
+  // SolidStart config
+  {
+    server: {
+      prerender: {
+        crawlLinks: true,
       },
     },
-    // SolidBase config
-    {
-      title: "Fullstack & Frontend Development",
-      titleTemplate: "Bruggen - :title",
-      description: "Jesse Dijkstra - Fullstack & Frontend Development",
-    }
-  )
+  }
 );
